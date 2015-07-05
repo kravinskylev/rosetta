8455b20873b9d0e4a8407909d2d460630aa765d7
Lev Kravinsky <kravinskylev@gmail.com>
2015-07-05 14:03:45 -0400

    clean up old code

--- rosetta.rb
+++ rosetta.rb
@@ -1,25 +1,11 @@
 require 'nokogiri'
 require 'net/http'
 
-#class RosettaCode
-#  def self.get_solution
    query        = ARGV[0]
-   # get HTML from the rosetta
    uri          = URI("http://rosettacode.org/wiki/" + query)
    body         = Net::HTTP.get(uri)
-   # parse it and use CSS selectors to find the ruby solution
    document     = Nokogiri::HTML(body)
    solutions    = document.css('.ruby')
-   # format the first solution from text/html into executable ruby
    simple_ruby_solution = solutions.first.text.split("  ").join("\n")
-   #puts simple_ruby_solution
-   #trying to fix lack of space before final end
    final_solution = simple_ruby_solution.gsub!(/end\Z/, " \n end")
-   #puts final_solution
-   #puts eval(final_solution)
    File.write("#{query}.rb", final_solution)
-
-#  end
-#end
-
-# RosettaCode.get_solution
