85752e8e1842cac9bde99a8ed442e3194b10d1e1
Lev Kravinsky <kravinskylev@gmail.com>
2015-07-05 14:28:20 -0400

    writes all solutions

--- rosetta.rb
+++ rosetta.rb
@@ -10,7 +10,12 @@ uri                 = URI("http://rosettacode.org/wiki/" + query)
 body                = Net::HTTP.get(uri)
 document            = Nokogiri::HTML(body)
 solutions           = document.css(".#{lang}")
-simple_solution     = solutions.map {|solution| puts solution.text.split("  ").join("\n") }
-File.write("#{query}.rb", simple_solution)
+simple_solutions     = solutions.map {|solution| solution.text.split("  ").join("\n") }
+#simple_solutions.map { |solution| File.write("#{query}.rb", solution) }
+open("#{query}.rb", 'a') { |f|
+  simple_solutions.map {|solution| f.puts(solution + "\n\n")}
+}
+
+#File.write("#{query}.rb", simple_solution)
 
 puts "A solution for #{query} has been written to #{query}.rb"
