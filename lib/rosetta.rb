a86f2ff6259915d50ba70553f7b2d7ddb7bbd5fe
Lev Kravinsky <kravinskylev@gmail.com>
2015-07-05 14:55:33 -0400

    put it in a module

--- lib/rosetta.rb
+++ lib/rosetta.rb
@@ -1,24 +1,24 @@
 require 'nokogiri'
 require 'net/http'
+module Lazy
+  class Rosetta
+    def self.search(query, lang="ruby")
+      puts "Your language is #{lang}, and your query is #{query}"
 
-query               = ARGV[0]
-ARGV[1].nil? ? lang = "ruby" : lang = ARGV[1]
+      uri                 = URI("http://rosettacode.org/wiki/" + query)
+      body                = Net::HTTP.get(uri)
+      document            = Nokogiri::HTML(body)
+      solutions           = document.css(".#{lang}")
+      simple_solutions    = solutions.map {|solution| solution.text.split("  ").join("\n") }
 
-puts "Your language is #{lang}, and your query is #{query}"
+      open("#{query}.rb", 'a') { |f|
+        simple_solutions.map {|solution| f.puts(solution + "\n\n")}
+      }
 
-uri                 = URI("http://rosettacode.org/wiki/" + query)
-body                = Net::HTTP.get(uri)
-document            = Nokogiri::HTML(body)
-solutions           = document.css(".#{lang}")
-simple_solutions    = solutions.map {|solution| solution.text.split("  ").join("\n") }
+      puts "A solution for #{query} has been written to #{query}.rb\n\n"
+      puts "Here is the first solution:\n\n #{simple_solutions.first}\n\n"
+    end
+  end
+end
 
-open("#{query}.rb", 'a') { |f|
-  simple_solutions.map {|solution| f.puts(solution + "\n\n")}
-}
-
-puts "A solution for #{query} has been written to #{query}.rb\n\n"
-puts "Here is the first solution:\n\n #{simple_solutions.first}\n\n"
-#TODO to have dictionary so that "JavaScript" is evaluated as "javascript"
-#TODO add message when no solution is found
-#TODO package as gem
-#TODO namespace
+Lazy::Rosetta.search("Fibonacci_sequence", "javascript")
