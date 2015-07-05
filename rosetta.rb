054649ce09350a2f02ab375508a37a8214b3dadd
Lev Kravinsky <kravinskylev@gmail.com>
2015-07-05 14:06:08 -0400

    first attempt at default lang

--- rosetta.rb
+++ rosetta.rb
@@ -2,10 +2,11 @@ require 'nokogiri'
 require 'net/http'
 
 query        = ARGV[0]
+lang         = ARGV[1] ? ARGV[1] : "ruby"
 uri          = URI("http://rosettacode.org/wiki/" + query)
 body         = Net::HTTP.get(uri)
 document     = Nokogiri::HTML(body)
-solutions    = document.css('.ruby')
+solutions    = document.css(".#{lang}")
 simple_ruby_solution = solutions.first.text.split("  ").join("\n")
 final_solution = simple_ruby_solution.gsub!(/end\Z/, " \n end")
 File.write("#{query}.rb", final_solution)
