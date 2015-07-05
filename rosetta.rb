0cab91fc4f315732408d85bcc3c6d16bbe0626b3
Lev Kravinsky <kravinskylev@gmail.com>
2015-07-05 14:22:41 -0400

    testing out writing all solutions

--- rosetta.rb
+++ rosetta.rb
@@ -1,13 +1,16 @@
 require 'nokogiri'
 require 'net/http'
+
 query               = ARGV[0]
 ARGV[1].nil? ? lang = "ruby" : lang = ARGV[1]
 
 puts "Your language is #{lang}, and your query is #{query}"
+
 uri                 = URI("http://rosettacode.org/wiki/" + query)
 body                = Net::HTTP.get(uri)
 document            = Nokogiri::HTML(body)
 solutions           = document.css(".#{lang}")
-simple_solution     = solutions.first.text.split("  ").join("\n")
+simple_solution     = solutions.map {|solution| puts solution.text.split("  ").join("\n") }
 File.write("#{query}.rb", simple_solution)
+
 puts "A solution for #{query} has been written to #{query}.rb"
