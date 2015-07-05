0b60a3af5ded31bcce271338245708ec7f438c97
Lev Kravinsky <kravinskylev@gmail.com>
2015-07-05 14:19:48 -0400

    better messages

--- rosetta.rb
+++ rosetta.rb
@@ -1,20 +1,13 @@
 require 'nokogiri'
 require 'net/http'
-
-query        = ARGV[0]
-
-#if ARGV[1].nil?
-#  lang = "ruby"
-#else
-#  lang = ARGV[1]
-#end
-
+query               = ARGV[0]
 ARGV[1].nil? ? lang = "ruby" : lang = ARGV[1]
 
 puts "Your language is #{lang}, and your query is #{query}"
-uri          = URI("http://rosettacode.org/wiki/" + query)
-body         = Net::HTTP.get(uri)
-document     = Nokogiri::HTML(body)
-solutions    = document.css(".#{lang}")
-simple_solution = solutions.first.text.split("  ").join("\n")
+uri                 = URI("http://rosettacode.org/wiki/" + query)
+body                = Net::HTTP.get(uri)
+document            = Nokogiri::HTML(body)
+solutions           = document.css(".#{lang}")
+simple_solution     = solutions.first.text.split("  ").join("\n")
 File.write("#{query}.rb", simple_solution)
+puts "A solution for #{query} has been written to #{query}.rb"
