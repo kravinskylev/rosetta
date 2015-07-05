e505e49525ab1f845c2484c487f42f65cc7b2826
Lev Kravinsky <kravinskylev@gmail.com>
2015-07-05 14:16:31 -0400

    ternary

--- rosetta.rb
+++ rosetta.rb
@@ -3,18 +3,18 @@ require 'net/http'
 
 query        = ARGV[0]
 
-if ARGV[1].nil?
-  lang = "ruby"
-else
-  lang = ARGV[1]
-end
+#if ARGV[1].nil?
+#  lang = "ruby"
+#else
+#  lang = ARGV[1]
+#end
 
-puts "your lang is #{lang}"
+ARGV[1].nil? ? lang = "ruby" : lang = ARGV[1]
+
+puts "Your language is #{lang}, and your query is #{query}"
 uri          = URI("http://rosettacode.org/wiki/" + query)
 body         = Net::HTTP.get(uri)
 document     = Nokogiri::HTML(body)
 solutions    = document.css(".#{lang}")
 simple_solution = solutions.first.text.split("  ").join("\n")
-#puts simple_solution
-#final_solution = simple_solution.gsub!(/end\Z/, " \n end")
 File.write("#{query}.rb", simple_solution)
