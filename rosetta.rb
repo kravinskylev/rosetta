0ee886bd734393653f2b5553e45db7335897655d
Lev Kravinsky <kravinskylev@gmail.com>
2015-07-05 14:04:03 -0400

    spacing

--- rosetta.rb
+++ rosetta.rb
@@ -1,11 +1,11 @@
 require 'nokogiri'
 require 'net/http'
 
-   query        = ARGV[0]
-   uri          = URI("http://rosettacode.org/wiki/" + query)
-   body         = Net::HTTP.get(uri)
-   document     = Nokogiri::HTML(body)
-   solutions    = document.css('.ruby')
-   simple_ruby_solution = solutions.first.text.split("  ").join("\n")
-   final_solution = simple_ruby_solution.gsub!(/end\Z/, " \n end")
-   File.write("#{query}.rb", final_solution)
+query        = ARGV[0]
+uri          = URI("http://rosettacode.org/wiki/" + query)
+body         = Net::HTTP.get(uri)
+document     = Nokogiri::HTML(body)
+solutions    = document.css('.ruby')
+simple_ruby_solution = solutions.first.text.split("  ").join("\n")
+final_solution = simple_ruby_solution.gsub!(/end\Z/, " \n end")
+File.write("#{query}.rb", final_solution)
