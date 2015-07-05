b816b9c8b55f52f26a1890fa7a643d9a8862b82a
Lev Kravinsky <kravinskylev@gmail.com>
2015-07-05 14:12:55 -0400

    trying out other langs

--- rosetta.rb
+++ rosetta.rb
@@ -9,12 +9,12 @@ else
   lang = ARGV[1]
 end
 
-
 puts "your lang is #{lang}"
 uri          = URI("http://rosettacode.org/wiki/" + query)
 body         = Net::HTTP.get(uri)
 document     = Nokogiri::HTML(body)
 solutions    = document.css(".#{lang}")
+puts solutions
 simple_ruby_solution = solutions.first.text.split("  ").join("\n")
 final_solution = simple_ruby_solution.gsub!(/end\Z/, " \n end")
 File.write("#{query}.rb", final_solution)
