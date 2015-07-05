b034dbe8da5f09a920e78e033170119cf4d8b009
Lev Kravinsky <kravinskylev@gmail.com>
2015-07-04 22:01:05 -0400

    proof of concept for ruby fizzbuzz

--- rosetta.rb
+++ rosetta.rb
@@ -1,2 +1,17 @@
 require 'nokogiri'
 require 'net/http'
+
+class RosettaCode
+  def self.get_fizzbuzz
+   # get HTML from the rosetta
+   uri          = URI("http://rosettacode.org/wiki/FizzBuzz")
+   body         = Net::HTTP.get(uri)
+   # parse it and use CSS selectors to find the ruby solution
+   document     = Nokogiri::HTML(body)
+   solutions    = document.css('.ruby')
+   # format the first solution from text/html into executable ruby
+   simple_ruby_solution = solutions.first.text.split("  ").join("\n")
+   final_solution = simple_ruby_solution.gsub("putsend", "return \n end")
+   puts eval(final_solution)
+  end
+end
